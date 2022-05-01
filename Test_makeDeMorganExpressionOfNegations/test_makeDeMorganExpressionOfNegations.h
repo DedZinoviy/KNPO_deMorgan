7675f509943f97354252ed56db86f0165fa87387
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-05-01 16:38:32 +0300

    Returned test API functions treeComparison and treeToList
    These functions, removed with the previous test_deMorgan test project, were checked in and added to the current test_makeDemorganExpressionOfnegations project.

--- Test_makeDeMorganExpressionOfNegations/test_makeDeMorganExpressionOfNegations.h
+++ Test_makeDeMorganExpressionOfNegations/test_makeDeMorganExpressionOfNegations.h
@@ -17,4 +17,21 @@ private slots:
     void test_case1();
 
 };
+
+
+/*!
+* \brief treeComparison copmpares two QDomNode trees.
+* \param firstTree - first tree for comparison.
+* \param secondTree - second tree for comparison.
+* \return sucсess of operation: true if success, false if failure.
+*/
+bool treeComparison(const QDomNode &firstTree, const QDomNode &secondTree);
+
+/*!
+* \brief treeToList decomposes the tree into a list by DFS.
+* \param tree - tree for decomposing.
+* \param treeList - result list of nodes.
+*/
+void treeToList(const QDomNode & tree, QList <QDomNode> &treeList);
+
 #endif // TEST_MAKEDEMORGANEXPRESSIONOFNEGATIONS_H
--- Test_makeDeMorganExpressionOfNegations/tst_test_makedemorganexpressionofnegations.cpp
+++ Test_makeDeMorganExpressionOfNegations/tst_test_makedemorganexpressionofnegations.cpp
@@ -10,6 +10,57 @@ test_makeDeMorganExpressionOfNegations::~test_makeDeMorganExpressionOfNegations(
 
 }
 
+bool treeComparison(const QDomNode &firstTree, const QDomNode &secondTree)
+{
+    QList <QDomNode> firstTreeList;
+    QList <QDomNode> secondTreeList;
+    treeToList(firstTree, firstTreeList); // Преобразовать первое дерево в список узлов.
+    treeToList(secondTree, secondTreeList); // Преобразовать второе дерево в список узлов.
+
+    bool isSameTree = true; // Считать, что изначально деревья одинаковые.
+    int firstListLen = firstTreeList.size();
+    int secListLen = secondTreeList.size();
+
+    for (int i = 0; i < firstListLen && i < secListLen; i++) // Для каждого элемента первого и второго списков...
+    {
+        QString firstTreeNodeTag = firstTreeList[i].toElement().tagName();
+        QString secTreeNodeTag = secondTreeList[i].toElement().tagName();
+        if (firstTreeNodeTag != secTreeNodeTag) // Считать узлы различными, если они имеют различные тэги, не являющийся тэгом операции.
+        {
+            isSameTree = false;
+            return isSameTree;
+        }
+        else // Иначе...
+        {
+            if (firstTreeNodeTag == "operation" ) // Проверить узлы на одинаковость значений аттрибута, если они имеют тэг операции.
+            {
+                QString firstNodeValue = firstTreeList[i].toElement().attributeNode("type").value();
+                QString secNodeValue = secondTreeList[i].toElement().attributeNode("type").value();
+                if(firstNodeValue != secNodeValue) // Считать узлы операции различными, если значения их аттрибутов не совпадают.
+                {
+                    isSameTree = false;
+                    return isSameTree;
+                }
+            }
+        }
+    }
+    return isSameTree;
+}
+
+void treeToList(const QDomNode & tree, QList <QDomNode> &treeList)
+{
+    treeList.append(tree); // Добавить текущий узел в список.
+    QDomNodeList childList = tree.childNodes(); // Получить дочерние узлы от текущего.
+    if (childList.isEmpty()) return; // Если их не существует, то прекратить запись.
+    else //Иначе...
+    {
+        for(int i = 0; i < childList.size(); i++) // Для каждого дочернего узла...
+        {
+            treeToList(childList.item(i), treeList); // декомпозировать поддерево в список.
+        }
+    }
+}
+
 void test_makeDeMorganExpressionOfNegations::test_case1()
 {
 
