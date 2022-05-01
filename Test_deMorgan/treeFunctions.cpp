19ceb4aa193ac42c484597e739cf66705f3ff717
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-05-01 11:05:44 +0300

    Added function API headers for further tree testing.
    Added a function header for comparing two trees treeComparison, a header for the function to decompose a tree into a treeToList list, and stubs in the corresponding .cpp file.

---
+++ Test_deMorgan/treeFunctions.cpp
@@ -0,0 +1,11 @@
+#include "treeFunctions.h"
+
+int treeComparison(const QDomNode &firstTree, const QDomNode &secondTree)
+{
+    return 0;
+}
+
+void treeToList(const QDomNode & tree, QList <QDomNode> &treeList)
+{
+
+}
---
+++ Test_deMorgan/treeFunctions.h
@@ -0,0 +1,22 @@
+#ifndef TREEFUNCTIONS_H
+#define TREEFUNCTIONS_H
+#include <QDomNode>
+#include <QList>
+
+
+/*!
+ * \brief treeComparison copmpares two QDomNode trees.
+ * \param firstTree - first tree for comparison.
+ * \param secondTree - second tree for comparison.
+ * \return sucсess of operation: 1 if success, 0 if failure.
+ */
+int treeComparison(const QDomNode &firstTree, const QDomNode &secondTree);
+
+/*!
+ * \brief treeToList decomposes the tree into a list by DFS.
+ * \param tree - tree for decomposing.
+ * \param treeList - result list of nodes.
+ */
+void treeToList(const QDomNode & tree, QList <QDomNode> &treeList);
+
+#endif // TREEFUNCTIONS_H
