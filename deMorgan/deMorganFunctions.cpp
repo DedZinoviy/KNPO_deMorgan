64176be07607b55118ee5ca2530e7ba9d4b00937
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-05-04 18:21:14 +0300

    Implemented makeDeMorganExpressionOfNegations function.
    The implemented function is located in the main project and allows you to create conjunctions or disjunctions of negations according to de Morgan's laws.

--- deMorgan/deMorganFunctions.cpp
+++ deMorgan/deMorganFunctions.cpp
@@ -3,9 +3,36 @@
 
 QDomNode makeDeMorganExpressionOfNegations(QDomNode &firstChild, QDomNode &secondChild, DeMorganLaws type)
 {
-    QDomDocument doc("");
-    QDomNode A = doc.createElement("operation");
-    return A;
+    QDomDocument doc("DeMorgan");
+
+        // Создать бинарную операцию согласно указанному закону Де Моргана
+        QDomElement operation = doc.createElement("operation");
+
+        switch (type)
+        {
+        case FIRST:  // создать дизъюнкцию, если указан первый закон
+            operation.setAttribute("type", "||");
+            break;
+        case SECOND: // Создать конъюнкцию, если указан второй закон
+            operation.setAttribute("type", "&&");
+            break;
+        }
+
+        // Создать отрицание первого операнда
+        QDomElement firstNegation = doc.createElement("operation");
+        firstNegation.setAttribute("type", "!");
+        firstNegation.appendChild(firstChild);
+
+        // создать отрицание второго операнда
+        QDomElement secondNegation = doc.createElement("operation");
+        secondNegation.setAttribute("type", "!");
+        secondNegation.appendChild(secondChild);
+
+        // Добавить созданные отрицания операндов как дочерние узлы к операции
+        operation.appendChild(firstNegation);
+        operation.appendChild(secondNegation);
+
+        return operation;
 }
 
 void makeConjunctiveDisjunctiveForm(QDomNode & node)
