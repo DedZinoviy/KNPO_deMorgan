7e47aa784dad7deadcc9750d3f7d791fdad30d28
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-05-05 23:48:36 +0300

    Added treeComparison method to the deMorganAndNegationsTransformatons test project
    A copy of the treeComparison API method from the makeDeMorganExpressionOfNegations test project for comparing two QDomNode trees has been added to the deMorganAndNegationsTransformations test project.

--- Test_deMorganAndNegationsTransformations/tst_test_demorganandnegationstransformations.cpp
+++ Test_deMorganAndNegationsTransformations/tst_test_demorganandnegationstransformations.cpp
@@ -34,6 +34,40 @@ void test_deMorganAndNegationsTransformations::treeToList(const QDomNode & tree,
         treeToList(childList.item(i), treeList); // декомпозировать поддерево в список.
     }
 }
+
+void test_deMorganAndNegationsTransformations::treeComparison(const QDomNode &firstTree, const QDomNode &secondTree)
+{
+
+    QList <QDomNode> firstTreeList;
+    QList <QDomNode> secondTreeList;
+    treeToList(firstTree, firstTreeList); // Преобразовать первое дерево в список узлов.
+    treeToList(secondTree, secondTreeList); // Преобразовать второе дерево в список узлов.
+
+    int firstListLen = firstTreeList.size();
+    int secListLen = secondTreeList.size();
+
+
+    for (int i = 0; i < firstListLen && i < secListLen; i++) // Для каждого элемента первого и второго списков...
+    {
+
+        QString firstTreeNodeTag = firstTreeList[i].toElement().tagName();
+        QString secTreeNodeTag = secondTreeList[i].toElement().tagName();
+
+
+        QCOMPARE(firstTreeNodeTag, secTreeNodeTag); // Сравнить имена тэговы
+
+            if (firstTreeNodeTag == "operation" ) // Проверить узлы на одинаковость значений аттрибута, если они имеют тэг операции.
+            {
+                QString firstNodeValue = firstTreeList[i].toElement().attributeNode("type").value();
+                QString secNodeValue = secondTreeList[i].toElement().attributeNode("type").value();
+
+                if(firstNodeValue != secNodeValue) // Считать узлы операции различными, если значения их аттрибутов не совпадают.
+                {
+                    QCOMPARE(firstNodeValue, secNodeValue);
+                }
+            }
+    }
+}
 void test_deMorganAndNegationsTransformations::test_case1()
 {
 
