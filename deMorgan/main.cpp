44987aa79e1bb3c65bb8668a9a6b47c4b48cb747
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2022-05-21 10:36:00 +0300

    Implemented main function, which solves the task.

--- deMorgan/main.cpp
+++ deMorgan/main.cpp
@@ -3,5 +3,29 @@
 
 int main(int argc, char *argv[])
 {
+    try
+    {
+        if (argc < 3) throw 16; // Сообщить об ошибке, если не хватает аргкментов при запуске программы.
+
+        // Начать работу с указанными файлами
+        FileWork file = FileWork();
+        QString inputName(argv[1]);
+        QString outputName(argv[2]);
+        file.setInputFile(inputName);
+        file.setOutputFile(outputName);
+
+        QDomNode firstNode = file.getFirstNode(); //Получить внешний узел входного дерева и всё его содержимое.
+
+        makeConjunctiveDisjunctiveForm(firstNode); // Преобразовать выражение к конъюнктивно-дизъюнктивной форме.
+
+        deMorganAndNegationTransformations(firstNode); // Удалить двойные отрицания и раскрыть по законам де Моргана
+
+        file.writeXmlFile(firstNode); // Записать изменённое дерево в выходной XMLфайл.
+    }
+    catch (int errorCode)
+    {
+        errorHandler(errorCode); // Обработать полученный код ошибки и вывести сообщение.
+        return errorCode;
+    }
     return 0;
 }
